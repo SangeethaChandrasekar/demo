f66d02dc617b66a60f48101b008d3b3f9b82c1c3
SangeethaChandrasekar <94848353+SangeethaChandrasekar@users.noreply.github.com>
2021-12-22 13:27:22 +0530

    Reversal of String

---
+++ Rv.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class Rv {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        String str= sc.nextLine();
+        char ch;
+        String rev="";
+        method(str);
+        if(str.matches("[a-z A-Z]+"))
+        {
+            for(int i=0;i<str.length();i++)
+            {
+                ch=str.charAt(i);
+                rev=ch+rev;
+            }
+            System.out.println(rev);
+        }
+        else
+        {
+            System.out.println("Error");
+        }
+    }
+    public static String method(String str){
+        return str;
+    }
+}
