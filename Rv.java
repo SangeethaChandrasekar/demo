eaa57d96a12b3d09f971ad8364eff3db29da6fe7
SangeethaChandrasekar <94848353+SangeethaChandrasekar@users.noreply.github.com>
2021-12-24 13:14:52 +0530

    Reverse String

--- Rv.java
+++ Rv.java
@@ -1,27 +1,30 @@
-import java.util.*;
-public class Rv {
-
-    public static void main(String[] args) {
-        Scanner sc= new Scanner(System.in);
-        String str= sc.nextLine();
+import java.util.Scanner;
+class Reverse {
+    public static String methodTwo(String s) {
         char ch;
-        String rev="";
-        method(str);
-        if(str.matches("[a-z A-Z]+"))
-        {
-            for(int i=0;i<str.length();i++)
-            {
-                ch=str.charAt(i);
-                rev=ch+rev;
+        String rev = "";
+        System.out.println("Given String:" + s);
+        if (s.matches("[a-z A-Z]+")) {
+            for (int i = 0; i < s.length(); i++) {
+                ch = s.charAt(i);
+                rev = ch + rev;
             }
-            System.out.println(rev);
-        }
-        else
-        {
+            System.out.println("Reversed String:" + rev);
+        } else {
             System.out.println("Error!!");
         }
+        return rev;
     }
-    public static String method(String str){
+}
+public class Rv {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        Reverse reverseString = new Reverse();
+        reverseString.methodTwo(str);
+        method(str);
+    }
+    public static String method (String str){
         return str;
     }
 }
